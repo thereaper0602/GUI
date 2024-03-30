ff62298cde9da3885e2e3e3ac49830e563c55987
Phi_Minh_Quang_2251012121 <130352764+thereaper0602@users.noreply.github.com>
2024-03-30 13:27:51 +0700

    Add files via upload

---
+++ raceGame/raceGame/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace raceGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.player2 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.player1 = new System.Windows.Forms.PictureBox();
+            this.winnerLb = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.second1 = new System.Windows.Forms.Label();
+            this.second2 = new System.Windows.Forms.Label();
+            this.second3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player3
+            // 
+            this.player3.BackColor = System.Drawing.Color.Silver;
+            this.player3.Location = new System.Drawing.Point(6, 6);
+            this.player3.Name = "player3";
+            this.player3.Size = new System.Drawing.Size(137, 127);
+            this.player3.TabIndex = 0;
+            this.player3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 80;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.player3);
+            this.panel1.Location = new System.Drawing.Point(-1, 576);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1042, 133);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel2.Controls.Add(this.player2);
+            this.panel2.Location = new System.Drawing.Point(2, 437);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1039, 133);
+            this.panel2.TabIndex = 1;
+            // 
+            // player2
+            // 
+            this.player2.BackColor = System.Drawing.Color.Red;
+            this.player2.Location = new System.Drawing.Point(3, 3);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(137, 127);
+            this.player2.TabIndex = 0;
+            this.player2.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panel3.Controls.Add(this.player1);
+            this.panel3.Location = new System.Drawing.Point(2, 298);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1039, 133);
+            this.panel3.TabIndex = 1;
+            // 
+            // player1
+            // 
+            this.player1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.player1.Location = new System.Drawing.Point(3, 3);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(137, 127);
+            this.player1.TabIndex = 0;
+            this.player1.TabStop = false;
+            // 
+            // winnerLb
+            // 
+            this.winnerLb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.winnerLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.winnerLb.Location = new System.Drawing.Point(206, 184);
+            this.winnerLb.Name = "winnerLb";
+            this.winnerLb.Size = new System.Drawing.Size(640, 79);
+            this.winnerLb.TabIndex = 2;
+            this.winnerLb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBox1.Location = new System.Drawing.Point(206, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 127);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox2.Location = new System.Drawing.Point(458, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(137, 127);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox3.Location = new System.Drawing.Point(713, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(137, 127);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // second1
+            // 
+            this.second1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.second1.Location = new System.Drawing.Point(206, 142);
+            this.second1.Name = "second1";
+            this.second1.Size = new System.Drawing.Size(137, 38);
+            this.second1.TabIndex = 3;
+            this.second1.Text = "0:00";
+            this.second1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // second2
+            // 
+            this.second2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.second2.Location = new System.Drawing.Point(458, 142);
+            this.second2.Name = "second2";
+            this.second2.Size = new System.Drawing.Size(137, 38);
+            this.second2.TabIndex = 3;
+            this.second2.Text = "0:00";
+            this.second2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // second3
+            // 
+            this.second3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.second3.Location = new System.Drawing.Point(709, 142);
+            this.second3.Name = "second3";
+            this.second3.Size = new System.Drawing.Size(137, 38);
+            this.second3.TabIndex = 3;
+            this.second3.Text = "0:00";
+            this.second3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1042, 712);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.second2);
+            this.Controls.Add(this.second3);
+            this.Controls.Add(this.second1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.winnerLb);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.player3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player2)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox player2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox player1;
+        private System.Windows.Forms.Label winnerLb;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label second1;
+        private System.Windows.Forms.Label second2;
+        private System.Windows.Forms.Label second3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ raceGame/raceGame/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace raceGame
+{
+    public partial class Form1 : Form
+    {
+        Random rand;
+        private Point oldPoint1, oldPoint2, oldPoint3;
+        private Stopwatch stopwatch;
+        private Timer timer;
+        private bool isMove;
+        private const int tick = 1000;
+        private const int V = 2000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == (Keys.Control | Keys.S)) {
+                stopwatch.Start();
+                timer.Start();
+                timer1.Enabled = true;
+            }
+            else if(e.KeyData == (Keys.Control | Keys.P))
+            {
+                stopwatch.Stop();
+                timer.Stop();
+                timer1.Enabled = false;
+            }
+            else if(e.KeyData == (Keys.Control | Keys.R))
+            {
+                player1.Location = oldPoint1;
+                player2.Location = oldPoint2;
+                player3.Location = oldPoint3;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stopwatch = new Stopwatch();
+            oldPoint1 = player1.Location; oldPoint2 = player2.Location; oldPoint2 = player2.Location;
+            timer = new Timer();
+            timer.Interval = 10;
+            this.KeyPreview = true;
+            rand = new Random();
+            isMove = timer1.Enabled;
+        }
+
+        private void Timer_Tick(object sender,EventArgs e)
+        {
+            second1.Text = $"{stopwatch.Elapsed.Seconds:D2}.{stopwatch.Elapsed.Milliseconds:D3}";
+            second2.Text = $"{stopwatch.Elapsed.Seconds:D2}.{stopwatch.Elapsed.Milliseconds:D3}";
+            second3.Text = $"{stopwatch.Elapsed.Seconds:D2}.{stopwatch.Elapsed.Milliseconds:D3}";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int move1 = rand.Next(10, 16);
+            int move2 = rand.Next(10, 16);
+            int move3 = rand.Next(10, 16);
+            player1.Left += move1;
+            player2.Left += move2;
+            player3.Left += move3;
+            timer.Tick += Timer_Tick;
+            if (player1.Right >= panel1.Width || player2.Right >= panel2.Width || player3.Right >= panel3.Width)
+            {
+                timer1.Enabled = false;
+                stopwatch.Stop();
+                timer.Stop();
+                if (player1.Right >= panel1.Width)
+                {
+                    winnerLb.Text = "Player1 Win";
+                }
+                else if (player2.Right >= panel2.Width)
+                {
+                    winnerLb.Text = "Player2 Win";
+                }
+                else if (player3.Right >= panel3.Width)
+                {
+                    winnerLb.Text = "Player3 Win";
+                }
+            }
+        }
+    }
+}
