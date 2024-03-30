6ee10a47dbfb968e90f3b42d0f7376210887a8b8
Phi_Minh_Quang_2251012121 <130352764+thereaper0602@users.noreply.github.com>
2024-03-30 13:28:27 +0700

    Add files via upload

---
+++ Clock_2/Clock_2/Clock.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Clock_2
+{
+    partial class Clock
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clockLb = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // clockLb
+            // 
+            this.clockLb.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.clockLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clockLb.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.clockLb.Location = new System.Drawing.Point(2, 2);
+            this.clockLb.Name = "clockLb";
+            this.clockLb.Size = new System.Drawing.Size(324, 148);
+            this.clockLb.TabIndex = 0;
+            this.clockLb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.clockLb);
+            this.Name = "Clock";
+            this.Size = new System.Drawing.Size(329, 150);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label clockLb;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Clock_2/Clock_2/Clock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock_2
+{
+    public partial class Clock : UserControl
+    {
+        public Clock()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            clockLb.Text = String.Format("{0}:{1:00}:{2:00}",
+                DateTime.Now.Hour,
+                DateTime.Now.Minute,
+                DateTime.Now.Second);
+        }
+    }
+}
