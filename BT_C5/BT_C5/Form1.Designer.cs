1e5a12f2fc41b53e305c3bda266ba23e9556b824
Phi_Minh_Quang_2251012121 <130352764+thereaper0602@users.noreply.github.com>
2024-04-14 23:31:07 +0700

    Add files via upload

---
+++ BT_C5/BT_C5/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace BT_C5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnBound = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // pnBound
+            // 
+            this.pnBound.AutoScroll = true;
+            this.pnBound.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.pnBound.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.pnBound.Location = new System.Drawing.Point(747, -5);
+            this.pnBound.Name = "pnBound";
+            this.pnBound.Size = new System.Drawing.Size(172, 638);
+            this.pnBound.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 632);
+            this.Controls.Add(this.pnBound);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel pnBound;
+    }
+}
+
---
+++ BT_C5/BT_C5/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BT_C5
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+        private Point OldPoint;
+        private String[] files;
+        Random rand = new Random();
+        private int imgSize = 120;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Lưu vị trí ban đầu khi click chuột lên
+            OldPoint = e.Location;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            files = Directory.GetFiles(Application.StartupPath + @"\Images\");
+            AddNewImage();
+        }
+
+        private void AddNewImage()
+        {
+            count++;// mục đích để đặt tên cho các pictureBox
+            PictureBox pic = new PictureBox();
+            Image img = Image.FromFile(files[rand.Next(files.Length)]);
+            pic.Image = img;
+            pic.Name = count.ToString(); // đặt tên
+            pic.SizeMode = PictureBoxSizeMode.StretchImage;
+            pic.Width = pic.Height = imgSize;
+            pic.MouseDown += Pic_MouseDown;// thêm sự kiện click chuột cho pictureBox
+            pic.MouseMove += Pic_MouseMove;
+            pic.MouseUp += Pic_MouseUp;
+            pic.Location = new Point(0, 0);
+            this.Controls.Add(pic);
+            /* @method SetchildIndex : đặt chỉ số cho 1 control cụ thể nào đó 
+             *                         để Controls.Count - 1 có nghĩa là để control đó thành 
+             *                         phần tử cuối cùng .Điều đó có nghĩa là nó sẽ bị các 
+             *                         control đè lên
+             */
+            this.Controls.SetChildIndex(pnBound, this.Controls.Count - 1);
+        }
+
+        private void Pic_MouseUp(object sender, MouseEventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            /**
+             * @ properties Bounds : trả về đối tượng là một Rectangle
+             * 
+             **/
+            if (pnBound.Bounds.Contains(pic.Bounds))
+            {
+                pnBound.Controls.Add(pic);
+                AddNewImage();
+            }
+        }
+
+        private void Pic_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                PictureBox pic = (PictureBox)sender;
+                int dx = e.X - OldPoint.X;
+                int dy = e.Y - OldPoint.Y;
+                pic.Left += dx;
+                pic.Top += dy;
+            }
+        }
+
+        private void Pic_MouseDown(object sender, MouseEventArgs e)
+        {
+            OldPoint = e.Location;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /** Control[] control : Tập hợp các control của form   
+             * @ method thís.Controls.Find(String name,bool) : Tham số đầu tiên : tìm một hoặc nhiều
+             *                                                 control phù hợp với tên tương ứng.
+             *                                                 Tham số thứ 2 chỉ định có kiếm các subcontrol
+             *                                                 của các control hay không . "false" là không
+             *                                                              
+            **/
+            Control[] control = this.Controls.Find(count.ToString(), false);
+            PictureBox pic = (PictureBox)control[0];
+            switch (e.KeyData)
+            {
+                case Keys.Left:
+                    {
+                        if(pic.Left > 0)
+                        {
+                            pic.Left-=20;
+                        }
+                        break;
+                    }
+                case Keys.Right:
+                    {
+                        if (pic.Right <= ClientRectangle.Width)
+                        {
+                            pic.Left+=20;
+                        }
+                        break;
+                    }
+                case Keys.Down:
+                    {
+                        if (pic.Bottom <= ClientRectangle.Height)
+                        {
+                            pic.Top+=20;
+                        }
+                        break;
+                    }
+                case Keys.Up:
+                    {
+                        if (pic.Top > 0)
+                        {
+                            pic.Top-=20;
+                        }
+                        break;
+                    }
+            }
+            if (pnBound.Bounds.Contains(pic.Bounds))
+            {
+                pnBound.Controls.Add(pic);
+                AddNewImage();
+            }
+        }
+    }
+}
