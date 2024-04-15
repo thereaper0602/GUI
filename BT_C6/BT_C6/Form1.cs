c467a7999b46686df97a68f8d89d2130c3c3f515
thereaper0602 <2251012121quang@ou.edu.vn>
2024-04-15 13:55:57 +0700

    Update

---
+++ BT_C6/BT_C6/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BT_C6
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] drives = Directory.GetLogicalDrives();
+            TreeNode node = null;
+            foreach(string drive in drives)
+            {
+                node = new TreeNode(drive);
+                treeView1.Nodes.Add(node);
+                
+            }
+        }
+    }
+}
